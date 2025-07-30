3cb769023e833851ef0e3337110e33848e7a6cd3
Ameyy01 <ameydabhole01@gmail.com>
2025-07-30 12:23:00 +0530

    Update Arr2DLL.cpp

--- LinkedLists/DoublyLL/Arr2DLL.cpp
+++ LinkedLists/DoublyLL/Arr2DLL.cpp
@@ -22,7 +22,7 @@ class Node {
 Node* convertArr2DLL(vector<int> &arr){
     Node*head = new Node(arr[0]);
     Node*prev = head;
-    for(int i=1; i<arr[i]; i++){
+    for(int i=1; i<arr.size(); i++){
         Node* temp = new Node(arr[i], nullptr, prev);
         prev-> next=temp;
         prev=temp;
